b6291fa002d5d38436124e9f133e78c23ba1cf56
Bình Minh <minhldb@gmail.com>
2021-09-17 23:14:47 +0700

    Add project files.

---
+++ WebApplicationMVC/Models/mvcEmployeeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMVC.Models
+{
+    public class mvcEmployeeModel
+    {
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Position { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Office { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
